6380d13103ed60b5179a45c6d2c483de75f911b5
kevalshah598 <kevalshah598@gmail.com>
2023-02-09 15:36:03 -0500

    enrollment phase 1

--- practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
@@ -15,8 +15,8 @@ class Enrollment < ApplicationRecord
         foreign_key: :course_id,
         class_name: :Course
 
-    has_many :users,
+    belongs_to :student,
         primary_key: :id,
-        foreign_key: :user_id,
+        foreign_key: :student_id,
         class_name: :User
 end
